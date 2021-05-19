1c38eb3af2f44191a34241d320fe03ec66d9f80b
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-05-19 19:47:43 -0400

    initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "258d49d84fmsh463a70f9e397e14p11e540jsnb40dba07f201",
-		"x-rapidapi-host": "streaming-availability.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+// fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "258d49d84fmsh463a70f9e397e14p11e540jsnb40dba07f201",
+// 		"x-rapidapi-host": "streaming-availability.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
