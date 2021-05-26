fb95f27e18508af0854a79adbcaebe02f8027c39
dillin92 <dillin970@gmail.com>
2021-05-25 21:05:26 -0400

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,23 @@ searchBtn.addEventListener('click', function getUserMovie() {
 
         });
         
+              //***API FOR WHERE TO FIND CONTENT USING TMDB ID***//
+      function getWhereToWatch() {
+        fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + sourceID + "&source=tmdb&country=us", {
+          "method": "GET",
+          "headers": {
+            "x-rapidapi-key": "45e2d343b0mshb21a1f4fd6e6d29p166b41jsnb915ef76a153",
+            "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+          }
+        }).then(function(response) {
+          response.json().then(function(data) {
+            console.log(data);
+          });
+          });
+        }
+
+        getWhereToWatch();
+        
 });
 
 function displayChoices() {
@@ -58,19 +75,6 @@ function displayChoices() {
 //EXAMPLE, 'SHAWSHANK REDEMPTION'
 let sourceID = "movie/278";
 
-//***API FOR WHERE TO FIND CONTENT USING TMDB ID***//
-function getWhereToWatch() {
-fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + sourceID + "&source=tmdb&country=us", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "45e2d343b0mshb21a1f4fd6e6d29p166b41jsnb915ef76a153",
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
-	}
-}).then(function(response) {
-	response.json().then(function(data) {
-	  console.log(data);
-	});
-  });
-}
-getWhereToWatch()
+
+//getWhereToWatch()
 
